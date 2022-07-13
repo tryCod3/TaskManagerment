3c62408a99f4e8508982196846e426594b71d8b2
tryCod3 <nthuy1999kadd1@gmail.com>
2022-07-13 15:20:08 +0700

    chore add webpack

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+  /**
+   * nhận boolean or list module
+   * if boolean = true : tất cả các file trong node_modules sẽ được đưa tới babel
+   * else if list module : sẽ chỉ định các file đó đưa đến babel
+   */
   transpileDependencies: true,
+  /**
+   * config webpack
+   */
+  configureWebpack: {},
 });
