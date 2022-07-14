04f3f895acb40f9454cff11ed97532908c8fbd79
tryCod3 <nthuy1999kadd1@gmail.com>
2022-07-14 11:27:48 +0700

    build: add Element Ui

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import Vue from "vue";
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import i18n from "@/lang";
+
+// element UI
+Vue.use(ElementUI, {
+	i18n: (key: string, value: string) => i18n.t(key, value),
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	i18n,
+	render: h => h(App),
 }).$mount("#app");
