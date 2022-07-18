f28c9dd9e2f6ad072f8abaf9ec1dc1eafb064de1
tryCod3 <nthuy1999kadd1@gmail.com>
2022-07-18 10:09:48 +0700

    feat(icon): add svg icon

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,21 @@ import '@/style/index.scss';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import SvgIcon from 'vue-svgicon';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from '@/lang';
 
 import '@/permission';
+import '@/icons/components';
+
+Vue.use(SvgIcon, {
+  tagName: 'svg-icon',
+  defaultWidth: '1em',
+  defaultHeight: '1em',
+});
 
 // element UI
 Vue.use(ElementUI, {
